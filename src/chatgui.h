51e21d10b19487326d9b6cad5f5467a3f2c8f14d
tasmanx <m.kuncevicius@gmail.com>
2020-04-17 09:21:55 +0300

    Replace raw pointer with unique_ptr & delete destructor which is no longer needed

--- src/chatgui.h
+++ src/chatgui.h
@@ -1,6 +1,7 @@
 #ifndef CHATGUI_H_
 #define CHATGUI_H_
 
+#include <memory>
 #include <wx/wx.h>
 
 class ChatLogic; // forward declaration
@@ -13,21 +14,14 @@ private:
     wxBoxSizer *_dialogSizer;
     wxBitmap _image;
 
-    //// STUDENT CODE
-    ////
-
-    ChatLogic *_chatLogic;
-
-    ////
-    //// EOF STUDENT CODE
+    std::unique_ptr<ChatLogic> _chatLogic = {};
 
 public:
-    // constructor / destructor
+    // constructor
     ChatBotPanelDialog(wxWindow *parent, wxWindowID id);
-    ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
